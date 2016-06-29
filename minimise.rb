45f51cdb4219217bd2064430f842139bac408981
robertburrowes <r.burrowes@auckland.ac.nz>
2016-06-29 20:15:44 +1200

    format comments

--- minimise.rb
+++ minimise.rb
@@ -1,7 +1,9 @@
 #!/usr/local/bin/ruby
-#Strips // comments from lines
-#Strips leading and trailing space on lines
-#Outputs any line still containing text.
+# Strips // comments from lines
+# Strips leading and trailing space on lines
+# Outputs any line still containing text.
+# Leaves new lines in text. Also leaves spaces within the line.
+# These add very little to the size of the -min file, while making error tracking easier.
 SOURCE = ARGV[0]
 TARGET = SOURCE.gsub(/\.js$/, "-min.js")
 File.open(SOURCE, "r") do |sfd|

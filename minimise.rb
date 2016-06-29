d6a90524bdcafc975ad110ed4a3156ee75b97ea3
Rob Burrowes <r.burrowes@auckland.ac.nz>
2016-06-29 13:54:15 +1200

    Update minimise.rb

--- minimise.rb
+++ minimise.rb
@@ -1,10 +1,17 @@
 #!/usr/local/bin/ruby
+# Strips // comments from lines
+# Strips leading and trailing space on lines
+# Outputs any line still containing text.
+# Leaves new lines in text. Also leaves spaces within the line.
+# These add very little to the size of the -min file, while making error tracking easier.
 SOURCE = ARGV[0]
 TARGET = SOURCE.gsub(/\.js$/, "-min.js")
 File.open(SOURCE, "r") do |sfd|
   File.open(TARGET, "w+") do |tfd|
     sfd.each_line do |l|
-      tfd.print l.chomp.strip.gsub(/\/\/.*$/, '')
+      new_line = l.strip.gsub(/\/\/.*$/, '')
+      tfd.puts new_line if new_line != ''
     end
   end
 end
+

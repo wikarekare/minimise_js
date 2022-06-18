378a427bd448fad7dc8871019d6f99824304861b
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-18 12:45:10 +1200

    use bash

--- minimise.rb
+++ minimise.rb
@@ -5,13 +5,12 @@
 # Leaves new lines in text. Also leaves spaces within the line.
 # These add very little to the size of the -min file, while making error tracking easier.
 SOURCE = ARGV[0]
-TARGET = SOURCE.gsub(/\.js$/, "-min.js")
-File.open(SOURCE, "r") do |sfd|
-  File.open(TARGET, "w+") do |tfd|
+TARGET = SOURCE.gsub(/\.js$/, '-min.js')
+File.open(SOURCE, 'r') do |sfd|
+  File.open(TARGET, 'w+') do |tfd|
     sfd.each_line do |l|
       new_line = l.strip.gsub(/\/\/.*$/, '')
       tfd.puts new_line if new_line != ''
     end
   end
 end
-
--- test/minimise.sh
+++ test/minimise.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/bash
 ../minimise.rb source_file.js
